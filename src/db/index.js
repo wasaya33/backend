d3f442efc69a97bd96f5e783a9635e5f837c5daa
Allah wasaya <awasaya058@gmail.com>
2024-01-31 22:31:20 +0500

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`\nMongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "../constants.js"
+
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI},${DB_NAME}`);
+//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+//     } catch (error) {
+//         console.log("MongoDB connection error " , error);
+//         process.exit(1);
+//     }
+// } 
+
+// export default connectDB
