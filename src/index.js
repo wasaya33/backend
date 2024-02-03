6a4664ffbe43ebc32b965a910b6820b1f9b45bc5
Allah wasaya <awasaya058@gmail.com>
2024-02-03 08:59:24 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ dotenv.config(
     {path:'./env'}
 );
 
+
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+   console.log("MongoDb connection failed : " , err);
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode , Data , message){
+        this.statusCode = statusCode
+        this.Data = Data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
